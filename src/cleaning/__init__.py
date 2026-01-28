58eeee4a19599c059f58041f4a0f5d29ef9216b1
Richard Elinam Nutsugah <richard.nutsugah@amalitech.com>
2026-01-28 10:55:37 +0000

    create init for cleanining data

--- src/cleaning/__init__.py
+++ src/cleaning/__init__.py
@@ -1,1 +1,29 @@
-"""Cleaning module"""
+"""
+Data cleaning module for TMDB movie data.
+
+Provides Spark-based cleaning and preprocessing functionality.
+"""
+
+from .cleaner import SparkMovieDataCleaner
+from .udfs import (
+    extract_collection_name,
+    extract_names_from_array,
+    extract_keywords_from_struct,
+    extract_top_cast,
+    get_cast_size,
+    extract_director,
+    get_crew_size,
+    sort_pipe_separated
+)
+
+__all__ = [
+    'SparkMovieDataCleaner',
+    'extract_collection_name',
+    'extract_names_from_array',
+    'extract_keywords_from_struct',
+    'extract_top_cast',
+    'get_cast_size',
+    'extract_director',
+    'get_crew_size',
+    'sort_pipe_separated'
+]
