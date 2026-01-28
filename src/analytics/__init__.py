6b768dec0659efd60f24ee795a1554e7251ac48d
Richard Elinam Nutsugah <richard.nutsugah@amalitech.com>
2026-01-28 10:58:47 +0000

    add init for analytics module

--- src/analytics/__init__.py
+++ src/analytics/__init__.py
@@ -1,1 +1,15 @@
-"""Analytics module"""
+"""
+Analytics module for TMDB movie data.
+
+Provides KPI calculations, filtering, aggregations, and performance analysis functionality.
+"""
+
+from .kpi_calculator import SparkKPICalculator
+from .filters import SparkMovieFilters
+from .aggregations import SparkMovieAggregations
+
+__all__ = [
+    'SparkKPICalculator',
+    'SparkMovieFilters',
+    'SparkMovieAggregations'
+]
