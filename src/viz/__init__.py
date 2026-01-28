10a0dd79e9006ebca506d19442aa9ddb2cffce86
Richard Elinam Nutsugah <richard.nutsugah@amalitech.com>
2026-01-28 13:55:47 +0000

    add visualization init

--- src/viz/__init__.py
+++ src/viz/__init__.py
@@ -1,1 +1,11 @@
-"""Visualization module"""
+"""
+Visualization module for TMDB movie data.
+
+Provides visualization functions for movie performance and trends.
+"""
+
+from .visualizations import MovieVisualizer
+
+__all__ = [
+    'MovieVisualizer'
+]
